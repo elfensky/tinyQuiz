e92a8b6a01c8e446264471c5a1cca8882d1212dc
Andrei Lavrenov <andrei.lavrenov@gmail.com>
2017-08-09 22:15:49 +0200

    merge "registration" into "master"

--- tinyQuiz.py
+++ tinyQuiz.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
+from flask import Flask, render_template, request, url_for, redirect, flash, session
 from dbClass import mySQL
 from flask_hashing import Hashing
 
@@ -14,6 +14,8 @@ def index():
             password = hashing.hash_value(attempted_password, salt='6825')
 
             user = mySQL().getDataFromCustomRow('tblusers', 'username', attempted_username)
+            # session['uid'] = user[0][0]
+
             if not user:
                 error="This user does not exist. Please try again."
                 flash(error)
@@ -27,14 +29,51 @@ def index():
         return render_template("index.html")
 
     except Exception as e:
-        error = "something went wrong"
-        flash(error)
+        error = "something went wrong: "
+        flash(error + str(e))
 
     return render_template('index.html')
 
 @app.route('/registration/')
 def registration():
-    return render_template('registration.html')
+    try:
+        if request.method == "POST":
+            attempted_username = request.form['username']
+            attempted_password1 = request.form['password1']
+            attempted_password2 = request.form['password2']
+            password = hashing.hash_value(attempted_password1, salt='6825')
+
+            user = mySQL().getDataFromCustomRow('tblusers', 'username', attempted_username)
+            print("attempted username: " + attempted_username)
+            # print("value of user list: " + str(user))
+            if attempted_username == '':
+                error = "Please enter a username"
+                flash(error)
+            else:
+                if user != []:
+                    error = "This username is taken. Please choose another."
+                    flash(error)
+                else:
+                    if attempted_password1 == '':
+                        error = "Please enter a password"
+                        flash(error)
+                    else:
+                        if attempted_password1 != attempted_password2:
+                            error = "Passwords do not match."
+                            flash(error)
+                        else:
+                            error = "you have successfully registered."
+                            flash(error)
+                            mySQL().setLoginDataToDatabase('tblusers', attempted_username, password)
+                            return redirect(url_for('quiz'))
+
+        return render_template("registration.html")
+
+    except Exception as e:
+        error = "something went wrong: "
+        flash(error + str(e))
+
+    return render_template('registration.html.html')
 
 @app.route('/quiz/')
 def quiz():
@@ -44,6 +83,33 @@ def quiz():
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.errorhandler(403)
+def forbidden():
+    return render_template("403.html")
+@app.errorhandler(404)
+def page_not_found():
+    return render_template("404.html")
+@app.errorhandler(405)
+def method_not_found():
+    return render_template("405.html")
+@app.errorhandler(500)
+def method_not_found():
+    return render_template("500.html")
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
